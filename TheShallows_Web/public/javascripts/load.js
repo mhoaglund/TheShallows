b4e7b4ef82a3640e12266461b4e5d411ae83bb67
mhoaglund <maxwell.hoaglund@gmail.com>
2017-07-01 19:08:43 -0500

    boilerplate

---
+++ TheShallows_Web/public/javascripts/load.js
@@ -0,0 +1,25 @@
+var data = {
+	active: initText,
+	google : {families: ['Cardo:italic', 'Roboto:300,100', 'Cutive Mono', 'Oranienbaum', 'Nunito Sans:900,700,500,300']}
+};
+
+ WebFont.load(data);
+$( window ).resize(function() {
+    setOalls();
+});
+
+var oallht;
+var oallwth;
+var oallctr;
+var useOverlay = true;
+var lightnessmod = 1.2;
+
+function initText(){
+	setOalls();
+}
+
+function setOalls(){
+	 oallht = $(window).height();
+	 oallwth = $(window).width(); 
+	 oallctr = {x: (oallwth/2), y: (oallht/2) };
+}
